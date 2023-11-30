e4aa5d8dce79b61b9bfd509f9282f9d5e1a114ad
FeiFei0827 <2035585231@qq.com>
2023-11-30 17:16:02 +0800

    env and test

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # minigrep
 使用rust来实现简单的grep命令
+
+powershell 使用$env:IGNORE_CASE=1;cargo run -- to poem.txt 来进行测试
+
+cmd bash 使用 IGNORE_CASE=1 cargo run -- to poem.txt
--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,28 @@
 use std::error::Error;
 use std::fs;
-
+use std::env;
 pub struct Config {
     pub query: String,
     pub file_path: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Config {
-        if args.len() < 3 {
-            panic!("\x1b[31mnot enough arguments\x1b[0m");
-        }//panic
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Config { query, file_path }
-    }
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
+    pub fn build(v:&[String]) -> Result<Config, &'static str> {
+        if v.len() < 3 {
             return Err("not enough arguments");
         }
+        let query = v[1].clone();
+        let file_path = v[2].clone();
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+        let ignore_case_flag = env::var("IGNORE_CASE").ok();
+        let ignore_case = match ignore_case_flag.as_ref().map(String::as_ref) {
+            None => false,
+            Some("0") => false,
+            Some(_) => true
+        };
 
-        Ok(Config { query, file_path })
+        Ok(Config { query, file_path, ignore_case })
     }
 }
 
@@ -39,18 +37,19 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    for line in search(&config.query, &contents) {
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{line}");
     }
 
     Ok(())
 }
-pub fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
 
-    Config { query, file_path }
-}
 // fn run(config: Config) {
 //     let contents = fs::read_to_string(config.file_path)
 //         .expect("Should have been able to read the file");
@@ -68,18 +67,49 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 
     results
 }
+pub fn search_case_insensitive<'a>(
+    query: &str,
+    contents: &'a str,
+) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "duct";
         let contents = "\
 Rust:
 safe, fast, productive.
-Pick three.";
+Pick three.
+Duct tape.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
     }
 }
